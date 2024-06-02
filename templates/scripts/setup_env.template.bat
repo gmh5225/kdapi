64b53516d0f78420d3a9c0c40d5968ced30bdee7
কুমারজিৎ দাস <kumarjitdas1999@gmail.com>
2024-06-02 23:19:42 +0530

    Reverted to c89 & using batch script for building.
    Removed all C99 components.
    Removed 'makefile'.
    Building using batch script.
    Build configurations added for Windows x86 and x64.
    Build tools support for MSVC, LLVM, and GNU.

---
+++ templates/scripts/setup_env.template.bat
@@ -0,0 +1,60 @@
+@REM file: setup_env.template.bat
+@REM author: Kumarjit Das
+@REM date: 2024-06-02
+@REM brief: A template for build environment setup.
+@REM
+@REM
+@REM LICENSE:
+@REM 
+@REM Copyright (c) 2024, Kumarjit Das
+@REM All rights reserved.
+@REM 
+@REM Redistribution and use in source and binary forms, with or without
+@REM modification, are permitted provided that the following conditions are met:
+@REM 
+@REM - Redistributions of source code must retain the above copyright notice, this
+@REM   list of conditions and the following disclaimer.
+@REM 
+@REM - Redistributions in binary form must reproduce the above copyright notice,
+@REM   this list of conditions and the following disclaimer in the documentation
+@REM   and/or other materials provided with the distribution.
+@REM 
+@REM THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+@REM AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+@REM IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+@REM DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+@REM FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+@REM DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+@REM SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+@REM CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+@REM OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+@REM OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+
+@REM SET KDAPI_HOST_ARCH=x86
+SET KDAPI_HOST_ARCH=x64
+
+@REM SET KDAPI_TARGET_ARCH=X86
+SET KDAPI_TARGET_ARCH=x64
+
+@REM SET KDAPI_COMPILER_PROVIDER=mingw
+SET KDAPI_COMPILER_PROVIDER=llvm
+@REM SET KDAPI_COMPILER_PROVIDER=msvc
+
+@REM SET KDAPI_BUILD_TOOLS_PATH=C:\Users\kumar\mingw32\bin
+@REM SET KDAPI_BUILD_TOOLS_PATH=C:\Users\kumar\mingw64\bin
+SET KDAPI_BUILD_TOOLS_PATH=C:\Users\kumar\llvm\bin
+@REM SET KDAPI_BUILD_TOOLS_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools
+
+@REM SET KDAPI_COMPILER_NAME=gcc.exe
+@REM SET KDAPI_COMPILER_NAME=clang.exe
+@REM SET KDAPI_COMPILER_NAME=cl.exe
+
+SET KDAPI_COMPILER_OPTIMIZE=true
+@REM SET KDAPI_COMPILER_OPTIMIZE=false
+
+@REM SET KDAPI_BUILD_TYPE=debug
